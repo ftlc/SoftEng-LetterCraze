9577f3e418eb364e4f93275ffb744c5ed925383e
jlhe97 <jlhe0197@gmail.com>
2016-11-30 03:45:37 -0500

    fix contributions and commits

--- Builder/Builder/Controllers/ExitBuilderController.java
+++ Builder/Builder/Controllers/ExitBuilderController.java
@@ -1,5 +1,24 @@
 package Builder.Controllers;
 
-public class ExitBuilderController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import Builder.Boundaries.LevelView;
+import Builder.Boundaries.SplashScreenView;
+
+public class ExitBuilderController implements ActionListener{
+	    SplashScreenView from;
+	    LevelView to;
+	    
+	    public ExitBuilderController(SplashScreenView from, LevelView to)
+	    {
+	        this.from = from;
+	        this.to = to;
+	    }
+
+	    @Override
+	    public void actionPerformed(ActionEvent actionEvent) {
+	        from.setVisible(false);
+	        to.setVisible(true);
+	    }
 }
