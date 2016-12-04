c5f5fe258df736ff740144fc29f14d2d3518bb3a
nivere <nicolette.vere@gmail.com>
2016-12-04 16:20:40 -0500

    Updated Weighted Letters
    Works better. Still working on the frequecies and getting making sure
    there are no null tiles.

--- Player/Player/Entities/Tile.java
+++ Player/Player/Entities/Tile.java
@@ -1,10 +1,11 @@
 package Player.Entities;
-
-import java.util.Random;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Iterator;
-
+import java.util.LinkedList;
 import java.util.Set;
+import java.util.Random;
+
 
 public class Tile{
 	String letter;
@@ -31,63 +32,92 @@ public class Tile{
 	}
 	// Generate Random Letters //
 	public String generateLetter(){
-		HashMap<Double, String> hm = new HashMap<Double, String>();
-		hm.put(8.167, "a");
-		hm.put(1.492, "b");
-		hm.put(2.782, "c");
-		hm.put(4.253, "d");
-		hm.put(12.702, "e");
-		hm.put(2.228, "f");
-		hm.put(2.015, "g");
-		hm.put(6.094, "h");
-		hm.put(6.966, "i");
-		hm.put(0.153, "j");
-		hm.put(0.772, "k");
-		hm.put(4.025, "l");
-		hm.put(2.406, "m");
-		hm.put(6.749, "n");
-		hm.put(7.507, "o");
-		hm.put(1.929, "p");
-		hm.put(0.095, "qu");
-		hm.put(5.987, "r");
-		hm.put(6.327, "s");
-		hm.put(9.056, "t");
-		hm.put(2.758, "u");
-		hm.put(0.978, "v");
-		hm.put(2.360, "w");
-		hm.put(0.150, "x");
-		hm.put(1.974, "y");
-		hm.put(0.074, "z");
+		HashMap<Integer, String> hm1 = new HashMap<Integer, String>();
+		hm1.put(1, "a");
+		hm1.put(2, "b");
+		hm1.put(3, "c");
+		hm1.put(4, "d");
+		hm1.put(5, "e");
+		hm1.put(6, "f");
+		hm1.put(7, "g");
+		hm1.put(8, "h");
+		hm1.put(9, "i");
+		hm1.put(10, "j");
+		hm1.put(11, "k");
+		hm1.put(12, "l");
+		hm1.put(13, "m");
+		hm1.put(14, "n");
+		hm1.put(15, "o");
+		hm1.put(16, "p");
+		hm1.put(17, "qu");
+		hm1.put(18, "r");
+		hm1.put(19, "s");
+		hm1.put(20, "t");
+		hm1.put(21, "u");
+		hm1.put(22, "v");
+		hm1.put(23, "w");
+		hm1.put(24, "x");
+		hm1.put(25, "y");
+		hm1.put(26, "z");
 		
-	     Set<Double> set = hm.keySet();
-	    
-	      double sum = 0;
-	      Iterator<Double> iterator = set.iterator();
-	      while(iterator.hasNext()){
-	        Double element = (Double)iterator.next();
-	        sum  = sum + element;
-	      }
+		LinkedList<Double> ll2 = new LinkedList<Double>();
+		LinkedList<Double> ll = new LinkedList<Double>();
+		ll.add(8.167);
+		ll.add(1.492);
+		ll.add(2.782);
+		ll.add(4.253);
+		ll.add(12.702);
+		ll.add(2.228);
+		ll.add(2.015);
+		ll.add(6.094);
+		ll.add(6.966);
+		ll.add(0.153);
+		ll.add(0.772);
+		ll.add(4.025);
+		ll.add(2.406);
+		ll.add(6.749);
+		ll.add(7.507);
+		ll.add(1.929);
+		ll.add(0.095);
+		ll.add(5.987);
+		ll.add(6.327);
+		ll.add(9.056);
+		ll.add(2.758);
+		ll.add(0.978);
+		ll.add(2.360);
+		ll.add(0.150);
+		ll.add(1.974);
+		ll.add(0.074);
+		int sw = 0;
+		String return_letter = null;
+		int sum  = 26;
+		Random r = new Random();
+		double randomValue = 0 + (100 - 0) * r.nextDouble();
 		
-	 	  Random rand = new Random(); 
-	 	  //give back a value between 0 and sum
-	      int value = rand.nextInt((int) (sum  + 1));
-	      
-	      Set<Double> set1 = hm.keySet();
-	    
-	      Iterator<Double> iterator1 = set1.iterator();
-	      int i = 0;
-	      while(iterator.hasNext()){
-	    	 
-	    	  Double element1 = (Double)iterator1.next();
-	    	  if(value < element1){
-	    		  //returns a letter
-	    	    return hm.get(i);
-	    	  }
-	    	  value-= element1;
-	    	  i++;
-	      }
-	      //should never get here
-		return hm.get(i);
+		double prev = 0;
+		for(int m = 0; m<sum ; m++){
+			sw  = 0;
+			double next	= prev + ll.get(m);
+			
+			ll2.add(next);
+			prev = next;
+			if(m == 25){
+				sw  = 1;
+			}
+		}
+	    int k = sum;
+	    if(sw == 1){
+	    for(k = sum -1 ; k>0 ; k--){
+	    	if(randomValue  <= ll2.get(k) & randomValue >ll2.get(k-1)){
+	    		return_letter = hm1.get(k);
+	    	}
+	    }
+	    }
+	
+
+	
+		return return_letter;
+
 	      
 	    	}
 
