90280b1e03407302767a90cf69ffbe903bf3fc0c
ftlc <harrysadoyan@gmail.com>
2016-12-15 21:14:19 -0500

    FINAL COMMIT

--- Builder/Builder/Controllers/PreviewLevelController.java
+++ Builder/Builder/Controllers/PreviewLevelController.java
@@ -34,83 +34,20 @@ public class PreviewLevelController implements ActionListener {
 	 * Method that launches a playable level preview.
 	 */
     public void actionPerformed(ActionEvent actionEvent) {
-    	Level theLevel = new Level();
-    	
-    	int starOne = Integer.parseInt(lv.getStarText(1).getText());
-    	int starTwo = Integer.parseInt(lv.getStarText(2).getText());
-    	int starThree = Integer.parseInt(lv.getStarText(3).getText());
-    	
-    	Star star = new Star(starOne, starTwo, starThree);
-    	theLevel.setStar(star);
-<<<<<<< HEAD
-    	
-    	String levelType = level.getLevelType();
-    			
-    	switch(levelType){
-    	case "Puzzle":
-    		PuzzleLevel pLevel = (PuzzleLevel)level;
-    		PuzzleLogic pLogic = new PuzzleLogic(theLevel);
-    		int maxWords = Integer.parseInt(lv.getMaxWordstxt().getText());
-    		theLevel.setDictionary(new Dictionary());
-    		
-    		pLogic.setMaxMoves(maxWords);
-    		pLogic.setMovesRemaining(maxWords);
-    		
-    		char[][] pLayout = getCharLayout(pLevel.getBoard());
-    		Player.Entities.Board pb = new Player.Entities.Board(pLayout);
-    		theLevel.setBoard(pb);
-    		
-    		
-    		theLevel.setLogic(pLogic);
-    		Player.Boundaries.LevelView plv = new Player.Boundaries.LevelView(theLevel);
-    		
-        	plv.setVisible(true);
-    		break;
-    	case "Lightning":
-    		LightningLevel lLevel = (LightningLevel)level;
-    		LightningLogic lLogic = new LightningLogic(theLevel);
-    		int timer = Integer.parseInt(lv.getTimeTxt().getText());
-    		theLevel.setDictionary(new Dictionary());
-    		lLogic.setTimer(timer);
-    		
-    		char[][] lLayout = getCharLayout(lLevel.getBoard());
-    		Player.Entities.Board lb = new Player.Entities.Board(lLayout);
-    		theLevel.setBoard(lb);
-    		
-    		theLevel.setLogic(lLogic);
-    		Player.Boundaries.LevelView llv = new Player.Boundaries.LevelView(theLevel);
-    		lLogic.startTimer(llv);
-        	llv.setVisible(true);
-    		break;
-    	case "Theme":
-    		ThemeLevel tLevel = (ThemeLevel)level;
-    		ThemeLogic tLogic = new ThemeLogic(theLevel);
-    		
-    		ArrayList<String> dict = tLevel.getDictionary().getWords();
-    		theLevel.setDictionary(new Player.Entities.Dictionary(true));
-    		for(String s: dict)
-    			theLevel.getDictionary().addWord(s);
-    		
-    		tLogic.setThemeName(lv.getTheme().getTheName());
-    		
-    		
-    		char[][] tLayout = tLevel.getDictionary().getLettersChar();
-    		Player.Entities.Board tb = new Player.Entities.Board(tLayout, true);
-    		theLevel.setBoard(tb);
-    		
-    		theLevel.setLogic(tLogic);
-    		Player.Boundaries.LevelView tlv = new Player.Boundaries.LevelView(theLevel);
-        	tlv.setVisible(true);
-    		break;
-    	}
-=======
->>>>>>> 50d0ae7f77421634e7dca49e1c6ae4d85b2d9d58
+		Level theLevel = new Level();
+
+		int starOne = Integer.parseInt(lv.getStarText(1).getText());
+		int starTwo = Integer.parseInt(lv.getStarText(2).getText());
+		int starThree = Integer.parseInt(lv.getStarText(3).getText());
+
+		Star star = new Star(starOne, starTwo, starThree);
+		theLevel.setStar(star);
 
 		String levelType = level.getLevelType();
 
-		switch(levelType){
+		switch (levelType) {
 			case "Puzzle":
-				PuzzleLevel pLevel = (PuzzleLevel)level;
+				PuzzleLevel pLevel = (PuzzleLevel) level;
 				PuzzleLogic pLogic = new PuzzleLogic(theLevel);
 				int maxWords = Integer.parseInt(lv.getMaxWordstxt().getText());
 				theLevel.setDictionary(new Dictionary());
@@ -129,7 +66,7 @@ public class PreviewLevelController implements ActionListener {
 				plv.setVisible(true);
 				break;
 			case "Lightning":
-				LightningLevel lLevel = (LightningLevel)level;
+				LightningLevel lLevel = (LightningLevel) level;
 				LightningLogic lLogic = new LightningLogic(theLevel);
 				int timer = Integer.parseInt(lv.getTimeTxt().getText());
 				theLevel.setDictionary(new Dictionary());
@@ -145,15 +82,15 @@ public class PreviewLevelController implements ActionListener {
 				llv.setVisible(true);
 				break;
 			case "Theme":
-				ThemeLevel tLevel = (ThemeLevel)level;
+				ThemeLevel tLevel = (ThemeLevel) level;
 				ThemeLogic tLogic = new ThemeLogic(theLevel);
 
 				ArrayList<String> dict = tLevel.getDictionary().getWords();
 				theLevel.setDictionary(new Player.Entities.Dictionary(true));
-				for(String s: dict)
+				for (String s : dict)
 					theLevel.getDictionary().addWord(s);
 
-				tLogic.setThemeName(lv.getTheme().getName());
+				tLogic.setThemeName(lv.getTheme().getTheName());
 
 
 				char[][] tLayout = tLevel.getDictionary().getLettersChar();
@@ -165,8 +102,9 @@ public class PreviewLevelController implements ActionListener {
 				tlv.setVisible(true);
 				break;
 		}
-
 	}
+
+
 
     /**
      * Get's a 2d array char layout from the board
